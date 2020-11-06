50745b42e3c34fb7090ae70de0492e5c6a25fdd6
Macbook <macbook@MacBook-Pro-Macbook.local>
2020-11-06 18:25:18 +0200

    exercise_4 has been done

---
+++ exercise_4/exercise_4.js
@@ -0,0 +1,30 @@
+function handleRequest(width, height) {
+  const img = `https://picsum.photos/${width}/${height}`;
+  console.log(img);
+  fetch(img)
+  .then(() => {
+    document.body.innerHTML += `
+      <br><br>
+      <img src=${img}></img>
+    `;
+    console.log("success");
+  })
+  .catch(error => {
+    alert(error);
+  })
+}
+
+document.querySelector("#btn24").addEventListener("click", () => {
+  const resolution = document.querySelector("#input24").value;
+  resolutionArr = resolution.split(" ");
+
+  const width = +resolutionArr[0];
+  const height = +resolutionArr[1];
+
+  if(width >= 100 && width <= 300 && height >= 100 && height <= 300) {
+    document.querySelector("#p24").innerHTML = "OK";
+    handleRequest(width, height);
+  } else {
+    document.querySelector("#p24").innerHTML = "One of the value is not in the range from 100 to 300";
+  }
+})
